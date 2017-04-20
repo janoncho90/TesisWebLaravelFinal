4ac440110741a69feeb8fd44c10f106d2b75e214
Fermin Mariante <Fermin Mariante>
2017-04-20 02:26:11 -0300

    agregue funciones(relaciones) al model user

--- app/User.php
+++ app/User.php
@@ -26,4 +26,44 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function rol()
+    {
+        return $this->belongsTo('App\Rol');
+    }
+
+    public function stores()
+    {
+        return $this->hasMany('App\Store');
+    }
+
+    public function addresses()
+    {
+        return $this->hasMany('App\Address');
+    }
+
+    public function califications_stores()
+    {
+        return $this->hasMany('App\Calification_Store');
+    }
+
+    public function comments_stores()
+    {
+        return $this->hasMany('App\Comment_Store');
+    }
+
+    public function candidates_notices()
+    {
+        return $this->hasMany('App\Candidate');
+    }
+
+    public function deliverymans_stores()
+    {
+        return $this->hasMany('App\Deliveryman_Store');
+    }
+
+    public function favorites_stores()
+    {
+        return $this->hasMany('App\Favorite_Store');
+    }
 }
